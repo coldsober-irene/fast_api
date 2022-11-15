f5ea66f9ade8e8bbb428e7f5d1ef1da93daab020
coldsober irene <hackapp78@gmail.com>
2022-11-15 15:22:59 +0200

    host web app on heruko

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 import pandas as pd
 import numpy as np
 from fastapi import Response
+import json
 
 app = FastAPI(title='my second fastapi')
 
@@ -13,17 +14,19 @@ data = {"names":['irene', 'coldsober','desire'],
         "occupation":["nothing", "coding", "not specified"]
         }
 
-df = pd.DataFrame([['irene','nothing',30],
-                    ['coldsober','coding',43],
-                    ['desire','not specified',50],
-                    ['wakanda','job',90],
-                    ['serevision','tendering',50],
-                    ['walter','not specified',70]
-                    ], columns=['names', 'occupations', 'age'])
-
+# df = pd.DataFrame([['irene','nothing',30],
+# ['coldsober','coding',43],
+# ['desire','not specified',50],
+# ['wakanda','job',90],
+# ['serevision','tendering',50],
+# ['walter','not specified',70]
+# ], columns=['names', 'occupations', 'age'])
+df = pd.DataFrame(data)
+# print(df.head())
 # @app.get('/pandas')
 # def show():
 #     return df.to_dict(orient = 'series')
 @app.get("/questions")
-def load_questions(orient:str='split'):
-    return Response(df.to_json(orient=orient), media_type="application/json") # split, values, index application/json specifies the output format to be json
+def load_questions(orient:str='split', cond):
+    j = df.to_json(orient=orient)
+    return Response(j, media_type="application/json") # split, values, index application/json specifies the output format to be json
