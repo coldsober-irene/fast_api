6c2e19b94fe27320a7dbcd0f4058f94b74fbb319
coldsober irene <hackapp78@gmail.com>
2022-11-15 15:57:42 +0200

    after handing 2nd error

--- api.py
+++ api.py
@@ -28,3 +28,5 @@ def names(first_name: bool = False, second_name: bool = False, full_name: bool =
         full_names = "Coldsober Irene"
     return full_names
 
+if __name == "__main__":
+    myApp.run()
